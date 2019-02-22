c13fc45c26fd3660419ff366a3016499f0582c6c
patrick.heenan@colorado.edu <patrick.heenan@colorado.edu>
2019-02-22 10:54:14 -0700

    added capability to use multiprocessing in boostrap.

--- bootstrap.py
+++ bootstrap.py
@@ -10,13 +10,36 @@ from scipy.interpolate import interp1d
 from scipy.optimize import curve_fit
 import multiprocessing
 
-def bootstrap(samples,function,n_trials,n_cpus=1,seed=None):
+
+def _adaptor_function(f,list_elements,args,kwargs):
+    """
+    :param f: function taking in list, then *args and **kwargs
+    :param list_elements: list-like
+    :param args: arguments
+    :param kwargs:  keyword arguments
+    :return: whatever f returns
+    """
+    return f(list_elements,*args,**kwargs)
+
+def _multiprocess_adapt(a):
+    """
+    :param a: assumed to be like <f, list_elements, args, kwargs>
+    :return: see _adaptor_function
+    """
+    return _adaptor_function(*a)
+
+
+def bootstrap(samples,function,n_trials,n_cpus=1,seed=None,args=[],
+              **kwargs):
     """
     :param samples: list-like of length n
-    :param function:  takes in list, returns whatever we want
+    :param function:  takes in list, then possible *args and ***kwargs,
+    returns whatever we want. for multiprocessing, should be pickleable
     :param n_trials:  number of bootstrapping trials to implement
     :param n_cpus: number of CPUs to use
     :param seed: if not None, seeds the PRNG
+    :param args: list, passed directly to function
+    :param kwargs: dictionary, passed directly to function
     :return: list of length n_trials, each element is function applied
     to samples using randomly drawn samples
     """
@@ -28,12 +51,14 @@ def bootstrap(samples,function,n_trials,n_cpus=1,seed=None):
         return
     if seed is not None:
         np.random.seed(seed)
-    sample_ensembles = [ np.random.choice(samples,size=n,replace=True)
+    sample_ensembles = [ [function,
+                          np.random.choice(samples,size=n,replace=True),
+                          args,kwargs]
                          for _ in range(n_trials)]
     if n_cpus > 1:
         Pool = multiprocessing.Pool(n_cpus)
         f_map = Pool.map
     else:
         f_map = map
-    to_ret = f_map(function,sample_ensembles)
+    to_ret = f_map(_multiprocess_adapt,sample_ensembles)
     return to_ret
